5f83d6a6cf7d9869962d45471f2a42bb321f4dd8
hulker <vegat.kevinj@gmail.com>
2021-01-20 07:45:25 -0400

    mi primera practica de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import tasks from './sample/tasks.json';
+
+// Components
+import Tasks from './components/Tasks';
+import TaskForm from './components/TaskForm';
+import Posts from './components/Posts';
+
+class App extends Component{
+    
+    state={
+        tasks:tasks
+    }
+    
+    addTask = (title, description) => {
+        const newTask = {
+            title: title,
+            description: description,
+            id: this.state.tasks.length
+        }
+        this.setState({
+            tasks:[...this.state.tasks, newTask] 
+        })
+    }
+    
+    deleteTask = id => {
+        const newTasks = this.state.tasks.filter(task => task.id !== id);
+        console.log(newTasks);
+        this.setState({tasks: newTasks})
+    }
+    
+    checkDone = id => {
+        const newTasks = this.state.tasks.map(task => {
+            if (task.id === id) {
+              task.done = !task.done
+            }
+            return task;
+        });
+        this.setState({tasks: newTasks})
+    }
+    
+    render(){
+        
+        return (
+            <div>
+                <Router>
+                   <Link to="/">Home</Link>
+                   <br/>
+                   <Link to="/posts">Posts</Link>
+                    <Route exact path="/" render={() => {
+                        return(                            
+                            <div>
+                                <TaskForm 
+                                    addTask={this.addTask}/>
+                                <Tasks 
+                                    tasks={this.state.tasks} 
+                                    deleteTask={this.deleteTask}
+                                    checkDone={this.checkDone}/>
+                            </div>
+                        )
+                    }}/>
+                    <Route path="/posts" component={Posts}/>
+                </Router>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App 
